19f450bdef7035c4ac5d5a340a25273f21f923ee
Andy Barrett <apbarret@nsidc.org>
2022-12-20 16:51:10 -0700

    initial commit

---
+++ ros_database/processing/concatenate_station_files.py
@@ -0,0 +1,5 @@
+"""Concatenates all files for a single station, creates a new filename with just 
+station code identifier and date range.  Note data are still only for winter period.
+This file format simplifies loading data and allows complete record download in the future.
+"""
+
