f36bd78d4ae876fa0d68cb9f1ede090a55796d10
Andy Barrett <apbarret@nsidc.org>
2022-12-27 17:02:26 -0700

    add glob and loop to processes all stations

--- ros_database/processing/clean_mesonet_data.py
+++ ros_database/processing/clean_mesonet_data.py
@@ -10,21 +10,15 @@ import pandas as pd
 from ros_database.processing.surface import (read_iowa_mesonet_file,
                                              parse_iowa_mesonet_file)
 from ros_database.processing.cleaning import remove_duplicate_records
-from ros_database.filepath import SURFOBS_RAW_PATH
+from ros_database.filepath import SURFOBS_CONCAT_PATH, SURFOBS_CLEAN_PATH
 
 from tests.test_fill import load_test_input
-
-country_list = ['alaska','canada','finland',
-                'greenland', 'iceland', 'norway',
-                'russia', 'sweden']
-
-pd.set_option('display.max_rows', None)
 
 def clean_iowa_mesonet_asos_station(station_path, verbose=False, debug=False):
     """Cleans raw Iowa Mesonet ASOS data for a single station.  All data files for a single
     station are combined.  Duplicate data records are removed.  Fields are converted
     from Imperial (English) units to SI.  Weather codes (WXCODE) for precipitation
-    type are interpretted and assigned to seprate boolean fields.  Data are written
+    type are interpretted and assigned to separate boolean fields.  Data are written
     to csv files.
 
     :station_path: Posix type or string type path to station files.
@@ -32,24 +26,43 @@ def clean_iowa_mesonet_asos_station(station_path, verbose=False, debug=False):
     :returns: None
     """
 
+    if verbose: print(f"    Loading data for {station_path}")
     if debug:
-        if verbose: print(f"    Loading data for {station_path}")
         df = load_test_input()
         outpath = "test_data_cleaned.csv"
     else:
-        df = read_iowa_mesonet_file(fp, usecols=None)
-    
+        df = read_iowa_mesonet_file(station_path, usecols=None)
+        outpath = f"{SURFOBS_CLEAN_PATH / station_path.stem}.clean.csv"
+
     if verbose: print("    Removing duplicate records...")
     df_cleaned = remove_duplicate_records(df)
     if verbose: print("    Parsing records, and converting units")
     df_parsed = parse_iowa_mesonet_file(df_cleaned)
 
     if verbose: print(f"    Writing cleaned data to {outpath}") 
-    df_cleaned.to_csv(outpath)
+    df_parsed.to_csv(outpath)
 
     return
 
 
+def clean_mesonet_data(verbose=False, debug=False):
+    """Cleans all stations in raw/all_stations directory
+
+    :verbose: verbose output
+    :debug: debug flag for testing
+    """
+    if debug:
+        filepaths = ["test data"]
+    else:
+        filepaths = SURFOBS_CONCAT_PATH.glob("*.csv")
+
+    if verbose: print("Cleaning mesonet observation data")
+    for fp in filepaths:
+        clean_iowa_mesonet_asos_station(fp, verbose=verbose, debug=debug)
+
+
 if __name__ == "__main__":
-    clean_iowa_mesonet_asos_station('test data', verbose=True)
-    #count_mesonet_duplicates()
+
+    verbose=True
+    debug = False
+    clean_mesonet_data(verbose=verbose, debug=debug)
