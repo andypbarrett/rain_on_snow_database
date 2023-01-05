f37686d9465bb6d9bb46cba0f7479184225f1c54
Andy Barrett <apbarret@nsidc.org>
2023-01-05 16:25:40 -0700

    kluge fix to suppress FurtureWarning

--- ros_database/processing/clean_mesonet_data.py
+++ ros_database/processing/clean_mesonet_data.py
@@ -5,6 +5,8 @@ Remove duplicates
 Remove rows where all NaN, except ID
 Convert to SI units
 """
+import warnings
+
 import pandas as pd
 
 from ros_database.processing.surface import (read_iowa_mesonet_file,
@@ -12,8 +14,15 @@ from ros_database.processing.surface import (read_iowa_mesonet_file,
 from ros_database.processing.cleaning import remove_duplicate_records
 from ros_database.filepath import SURFOBS_CONCAT_PATH, SURFOBS_CLEAN_PATH
 
+# Suppresses FutureWarning about conflict in how strings and scalars are compared
+# see: https://stackoverflow.com/questions/40659212/futurewarning-elementwise-comparison-failed-returning-scalar-but-in-the-futur
+# Using
+# python                    3.7.6           h8356626_5_cpython    conda-forge
+# numpy                     1.18.4           py37h8960a57_0    conda-forge
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
-def clean_iowa_mesonet_asos_station(station_path, verbose=False):
+def clean_iowa_mesonet_asos_station(station_path, verbose=False,
+                                    ignore_fill_warnings=False):
     """Cleans raw Iowa Mesonet ASOS data for a single station.  All data files for a single
     station are combined.  Duplicate data records are removed.  Fields are converted
     from Imperial (English) units to SI.  Weather codes (WXCODE) for precipitation
