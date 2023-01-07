9b1ab75fa21e3f8e3b4d857823cb7bed4dfdd8f4
Andy Barrett <apbarret@nsidc.org>
2023-01-07 14:29:15 -0700

    add test for renage d2m

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -198,6 +198,14 @@ def test_expected_range_t2m():
     assert expected.equals(parse), f"Expected {expected['t2m'].values}, got {parse['t2m'].values}"
 
 
+def test_expected_range_d2m():
+    """Test for relh range check"""
+    parse = pd.DataFrame({'d2m': [np.nan, -100., -70., 0., 20., 50., 70]})
+    expected = pd.DataFrame({'d2m': [np.nan, np.nan, -70., 0., 20., 50., np.nan]})
+    range_check_var(parse, 'd2m')
+    assert expected.equals(parse), f"Expected {expected['d2m'].values}, got {parse['d2m'].values}"
+
+
 def test_parse_dataframe_trace():
     """Test correct parsing of df_with_trace"""
     df_parse = parse_iowa_mesonet_file(df_with_trace)
